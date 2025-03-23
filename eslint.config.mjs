35dbbbec021499f8efa285ad541aa60265b1525b
phanduy <duydp.dev@gmail.com>
2025-03-23 11:37:14 +0700

    [conf] add config & lib

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactHooks from 'eslint-plugin-react-hooks';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript","prettier","plugin:@typescript-eslint/recommended",  ),
+  ...compat.config({
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ],
+      "no-unused-vars": [
+        "warn", {
+          "argsIgnorePattern": "^_"}
+      ],
+    }
+  }),
 ];
 
 export default eslintConfig;
